639f285606d4c77a8877721994d3ceadbcb39ffb
Krishna24s <ks34085@gmail.com>
2018-03-24 23:03:41 +0530

    First commit

---
+++ Spy_details.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+class Spy:
+
+    def __init__(self, name, salutation, age, rating):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.is_online = True
+        self.chats = []
+        self.current_status_message = None
+
+spy = Spy('Krishna', 'Mr.', 21, 4.7)
---
+++ main.py
@@ -0,0 +1,8 @@
+from Spy_details import spy, Spy
+from datetime import datetime
+
+print "Hello!"
+
+print'let\'s get started'
+question = "Do you want to continue as " + spy.salutation + " " + spy.name + " (Y/N)? "
+existing = raw_input(question)
